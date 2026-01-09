b18fd16393ada94e203e694e21c4bda5a458c3bb
iamjoramcastro <noiwillnotgiveittoyou@gmail.com>
2026-01-09 23:14:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,46 @@
-// Define a name for the cache
-const CACHE_NAME = 'cnhs-file-depot-v1';
+const CACHE_NAME = 'cnhs-file-depot-v2';
 
-// List the files to cache. Since CSS and JS are inline, we only need the main page and the logo.
 const urlsToCache = [
   '/',
   'index.html',
-  'https://raw.githubusercontent.com/gu3sswh4t/COGONNATIONALHIGHSCHOOL/main/images/cogon.png'
+  'https://raw.githubusercontent.com/gu3sswh4t/COGONNATIONALHIGHSCHOOL/main/images/cogon.png',
+  'https://cdn.jsdelivr.net/npm/tsparticles@2.11.1/tsparticles.bundle.min.js'
 ];
 
-// Install event: fires when the service worker is first installed.
 self.addEventListener('install', (event) => {
-  // We wait until the installation is complete.
   event.waitUntil(
-    // Open the cache.
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
-        // Add all the specified files to the cache.
         return cache.addAll(urlsToCache);
       })
   );
+  self.skipWaiting();
 });
 
-// Fetch event: fires for every network request made by the page.
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    // Check if the request is already in our cache.
     caches.match(event.request)
       .then((response) => {
-        // If we have a cached response, return it.
         if (response) {
           return response;
         }
-        // If not, fetch the request from the network.
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
